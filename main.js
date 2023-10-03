f0d7fb95b1766646f68899e898e26b52e1a8db70
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-10-03 12:21:35 -0500

    added notes

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 console.log("Random Question: Do you prefer coffee or tea?");
 
+//Object
 const myDog = {
   name: "Kallum",
   age: 3,
   breed: "Border Collie",
   temperment: "hyper",
 };
+
+document.getElementById("test1").innerHTML = myDog;
+
+// Object Method
+
+//Array
+
+//Array Method
