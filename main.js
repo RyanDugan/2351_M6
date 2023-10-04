6af4e30d2563f6dafeb2ee1ddae4a6c7b19dc231
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-10-04 15:54:08 -0500

    added object with this keyword

--- main.js
+++ main.js
@@ -30,7 +30,24 @@ delete myDog.breed;
 
 document.getElementById("test1.4").innerHTML =
   myDog.name + " is a " + myDog.breed;
+
 // Object Method
+const legendOfZelda = {
+  releaseDate: "Februrary 21, 1986 (Japan)",
+  publisher: "Nintendo",
+  creator: "Shigeru Miyamoto",
+  fullStatement: function () {
+    return (
+      this.creator +
+      " released the first Legend of Zelda title on " +
+      this.releaseDate +
+      " with " +
+      this.publisher
+    );
+  },
+};
+
+document.getElementById("test2").innerHTML = legendOfZelda.fullStatement();
 
 //Array
 
